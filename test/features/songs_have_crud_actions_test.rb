de35989517fc417b1e14cb72f85c538a558e6992
Scott Macpherson <scottmacphersonmusic@gmail.com>
2015-09-15 21:59:24 -0700

    Spec unhappy paths for create and update song

--- test/features/songs_have_crud_actions_test.rb
+++ test/features/songs_have_crud_actions_test.rb
@@ -9,12 +9,24 @@ feature "songs have crud actions" do
     fill_in "Title", with: "Solar"
     fill_in "Composer", with: "Miles Davis"
     click_on "Create Song"
-    # Then a new song is created
+    # Then a new song is created and I see a notice
     page.must_have_content "New song created!"
     page.must_have_content "Miles Davis"
   end
 
-  scenario "edit song" do
+  scenario "attempt to create invalid song" do
+    # Given a new song form
+    visit new_song_path
+    # When I try to create a new, invalid song
+    fill_in "Title", with: ""
+    fill_in "Composer", with: ""
+    # The song is not created and I see an error message
+    current_path.must_equal new_song_path
+    page.must_have_content "Song not created."
+    page.must_have_content "?error?"
+  end
+
+  scenario "update song" do
     # Given an existing song
     visit root_path
     # When I click 'edit' and submit some changes
@@ -24,6 +36,18 @@ feature "songs have crud actions" do
     # The song is edited
     page.must_have_content "Song successfully updated!"
     page.must_have_content "Share-a-key"
+  end
+
+  scenario "attempt to edit invalid song" do
+    # Given a new song edit form
+    song = songs(:all_the_things)
+    visit edit_song_path(song)
+    # When I try update an invalid song
+    fill_in "Composer", with: ""
+    # The song is not created and I see an error message
+    current_path.must_equal edit_song_path(song)
+    page.must_have_content "Song not updated."
+    page.must_have_content "?error?"
   end
 
   scenario "delete song" do
