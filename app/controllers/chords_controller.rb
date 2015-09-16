23300cba7e3fedcaf487a8c25c8179999c3a2813
Scott Macpherson <scottmacphersonmusic@gmail.com>
2015-09-16 14:26:15 -0700

    Pass update chord spec
    Add 'before_action's to chord controller
    Add update method to chord controller
    Revise chord edit view
    Add crud links to chord show view
    Revise update chord spec
    Add 'song' attribute to chord fixtures

--- app/controllers/chords_controller.rb
+++ app/controllers/chords_controller.rb
@@ -1,19 +1,19 @@
 class ChordsController < ApplicationController
+  before_action :set_song
+  before_action :set_chord, only: [:show, :edit]
+
   def show
-    @chord = Chord.find(params[:id])
   end
 
   def new
     @chord = Chord.new
-    @song = Song.find(params[:song_id])
   end
 
   def create
-    @song = Song.find(params[:song_id])
     @chord = @song.chords.build(chord_params)
     if @chord.save
       flash[:notice] = "Chord successfully created!"
-      redirect_to @song
+      redirect_to song_chord_path(@song, @chord)
     else
       render 'new'
     end
@@ -22,9 +22,27 @@ class ChordsController < ApplicationController
   def edit
   end
 
+  def update
+    @chord = @song.chords.build(chord_params)
+    if @chord.save
+      flash[:notice] = "Chord successfully updated!"
+      redirect_to song_chord_path(@song, @chord)
+    else
+      render 'edit'
+    end
+  end
+
   private
 
   def chord_params
     params.require(:chord).permit(:pitch, :quality, :measure, :sequence)
+  end
+
+  def set_song
+    @song = Song.find(params[:song_id])
+  end
+
+  def set_chord
+    @chord = Chord.find(params[:id])
   end
 end
--- test/features/chords_have_crud_actions_test.rb
+++ test/features/chords_have_crud_actions_test.rb
@@ -12,12 +12,24 @@ feature "chords have crud actions" do
     fill_in "Sequence", with: "1"
     click_on "Create Chord"
     # A chord is created
-    save_and_open_page
     page.must_have_content "Chord successfully created!"
     page.must_have_content "Bbmaj7"
   end
 
   scenario "update chord" do
+    # Given an existing chord
+    song = songs(:cherokee)
+    chord = chords(:cher_1_1)
+    visit song_chord_path(song, chord)
+    # When I click on edit, fill out and submit the form
+    click_on "Edit"
+    fill_in "Pitch", with: "A#"
+    fill_in "Quality", with: "7(#9)"
+    click_on "Update Chord"
+    # Then the chord is edited
+    page.must_have_content "Chord successfully updated!"
+    page.must_have_content "A#"
+    page.must_have_content "7(#9)"
   end
 
   scenario "delete chord" do
